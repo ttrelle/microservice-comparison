18c1d3010a73dc9e6ca26485ddb78fed9a86a606
Tobias Trelle <tobias.trelle@codecentric.de>
2018-12-11 15:04:05 +0100

    added missing Webflux controller

---
+++ spring-boot-webflux/src/main/java/de/codecentric/boot/webflux/SimpleWebfluxController.java
@@ -0,0 +1,15 @@
+package de.codecentric.boot.webflux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SimpleWebfluxController {
+
+    @GetMapping("/hello")
+    public Mono<String> hello() {
+        return Mono.just("hello world");
+    }
+
+}
