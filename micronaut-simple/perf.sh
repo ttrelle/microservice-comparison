55931525d043edd67afd7797ef151c799241741c
Tobias Trelle <tobias.trelle@codecentric.de>
2019-02-15 14:24:20 +0100

    shell scripts for measuring startup times

---
+++ micronaut-simple/perf.sh
@@ -0,0 +1,22 @@
+LOG=startup.log
+N=$1
+
+echo "Doing $N iteration/s ..."
+
+# start / stop application several times
+if [ -f $LOG ]; then rm $LOG; fi
+for i in `seq 1 $N`; do
+    java -jar target/micronaut-simple-0.1.jar >> $LOG &
+    PID=$!
+    sleep 6
+    kill -9 $PID > /dev/null
+done
+
+# calculate mean startup time
+sum=0;
+count=0;
+for i in $(awk 'match($0, /in (.+)ms/) {print substr($0, RSTART+3, RLENGTH-5)}' $LOG); do
+    sum=$(echo $sum+$i | bc)
+    ((count++))
+done
+echo "scale=4; $sum / $count / 1000" | bc
---
+++ spring-boot-webflux-demo/perf.sh
@@ -0,0 +1,22 @@
+LOG=startup.log
+N=$1
+
+echo "Doing $N iteration/s ..."
+
+# start / stop application several times
+if [ -f $LOG ]; then rm $LOG; fi
+for i in `seq 1 $N`; do
+    java -jar target/spring-boot-webflux-demo-0.0.1.jar >> $LOG &
+    PID=$!
+    sleep 6
+    kill $PID
+done
+
+# calculate mean startup time
+sum=0;
+count=0;
+for i in $(awk 'match($0, /in (.+) seconds/) {print substr($0, RSTART+3, RLENGTH-11)}' $LOG); do
+    sum=$(echo $sum+$i | bc)
+    ((count++))
+done
+echo "scale=4; $sum / $count" | bc
